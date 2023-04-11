1a2c438df34f91f83675a3b16ac6b4445f28ed11
Albert Boehmler <ajboehmler@gmail.com>
2023-04-11 00:15:40 -0400

    Web installer manifest work

--- sites/site-www-qroma-project/docusaurus.config.js
+++ sites/site-www-qroma-project/docusaurus.config.js
@@ -32,6 +32,14 @@ const config = {
     locales: ['en'],
   },
 
+  scripts: [
+    // 'https://unpkg.com/esp-web-tools@9.2.1/dist/web/install-button.js?module'
+    {
+      src: '/esp-web-tools/install-button.js?module',
+      async: true,
+    },
+  ],
+
   presets: [
     [
       'classic',
---
+++ sites/site-www-qroma-project/static/esp-web-tools/README.md
@@ -0,0 +1,5 @@
+#esp-web-tools
+
+Content here comes from the [ESP Web Tools preojct](https://esphome.github.io/esp-web-tools/)
+
+Files come from version 9.2.1 - https://unpkg.com/esp-web-tools@9.2.1/dist/web/install-button.js?module
