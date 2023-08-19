76530fe34c923114bfda8d41946c83aee1770757
Albert Boehmler <ajboehmler@gmail.com>
2023-08-19 11:35:21 -0400

    Fix for the #include fix

--- firmware/esp32/qroma-project/src/qroma-project/qroma-commands.h
+++ firmware/esp32/qroma-project/src/qroma-project/qroma-commands.h
@@ -1,7 +1,7 @@
 #ifndef QROMA_COMMANDS_H
 #define QROMA_COMMANDS_H
 
-#include </qroma-proto/hello-qroma.pb.h>
+#include <qroma-proto/hello-qroma.pb.h>
 
 void onMyAppCommand(MyAppCommand * message, MyAppResponse * hqr);
 
