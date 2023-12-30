1b172cbb42861cc43b02e241346cbad87908a661
Albert Boehmler <ajboehmler@gmail.com>
2023-12-30 16:23:54 -0500

    Tweaks for single file firmware projects

---
+++ firmware/esp32/qroma-project/build_firmware_file.py
@@ -0,0 +1,83 @@
+# based on code and comments found here: https://github.com/platformio/platform-espressif32/issues/1078
+
+Import("env")
+import os
+
+APP_BIN = "$BUILD_DIR/${PROGNAME}.bin"
+BOARD_CONFIG = env.BoardConfig()
+# print(env.Dump())
+
+print(dir(BOARD_CONFIG))
+board_variant = BOARD_CONFIG.get("build.variant")
+print("MERGING FIRMWARE FOR BOARD '" + board_variant + "'")
+
+PROJECT_ID = "{{ qroma_project.project_id }}"
+MERGED_BIN_FILENAME = f"{PROJECT_ID}-firmware-{board_variant}.bin"
+
+MERGED_BIN = f"$BUILD_DIR/{MERGED_BIN_FILENAME}"
+CHIP_FAMILY = BOARD_CONFIG.get("build.mcu", "esp32")
+
+
+def merge_bin(source, target, env):
+    # The list contains all extra images (bootloader, partitions, eboot) and
+    # the final application binary
+    flash_images = env.Flatten(env.get("FLASH_EXTRA_IMAGES", [])) + ["$ESP32_APP_OFFSET", APP_BIN]
+
+    # Run esptool to merge images into a single binary
+    env.Execute(
+        " ".join(
+            [
+                "$PYTHONEXE",
+                "$OBJCOPY",
+                "--chip",
+                CHIP_FAMILY,
+                "merge_bin",
+                # "--fill-flash-size",
+                # BOARD_CONFIG.get("upload.flash_size", "4MB"),
+                "-o",
+                MERGED_BIN,
+            ]
+            + flash_images
+        )
+    )
+
+
+def create_esp_web_tools_manifest(source, target, env):
+    print("TARGET")
+    firmware_path = target[0].get_abspath()
+    firmware_dir = os.path.dirname(firmware_path)
+    print(firmware_dir)
+    manifest_json = f"""{{
+  "name": "{PROJECT_ID}",
+  "version": "esp32",
+  "builds": [
+    {{
+      "chipFamily": "{CHIP_FAMILY}",
+      "parts": [
+          {{ "path": "{MERGED_BIN}", "offset": 0 }},
+      ]
+    }},
+  ]
+}}"""
+    print(manifest_json)
+
+    manifest_filename = f"{firmware_dir}/{PROJECT_ID}-manifest-{board_variant}.json"
+    print("WRITING MANIFEST TO " + manifest_filename)
+    with open(manifest_filename, "w") as f:
+        f.write(manifest_json)
+
+
+# Add a post action that runs esptoolpy to merge available flash images
+env.AddPostAction(APP_BIN , merge_bin)
+env.AddPostAction(APP_BIN, create_esp_web_tools_manifest)
+
+# Patch the upload command to flash the merged binary at address 0x0
+env.Replace(
+    UPLOADERFLAGS=[
+            f
+            for f in env.get("UPLOADERFLAGS")
+            if f not in env.Flatten(env.get("FLASH_EXTRA_IMAGES"))
+        ]
+        + ["0x0", MERGED_BIN],
+    UPLOADCMD='"$PYTHONEXE" "$UPLOADER" $UPLOADERFLAGS',
+)
