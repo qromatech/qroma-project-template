6ded0b719561b6a2a124520ffbcaaa8d5f83b8fc
Albert Boehmler <ajboehmler@gmail.com>
2023-12-30 22:43:19 -0500

    put some stuff back

--- firmware/esp32/qroma-project/build_firmware_file.py
+++ firmware/esp32/qroma-project/build_firmware_file.py
@@ -17,3 +17,26 @@ MERGED_BIN_FILENAME = f"{PROJECT_ID}-firmware-{board_variant}.bin"
 MERGED_BIN = f"$BUILD_DIR/{MERGED_BIN_FILENAME}"
 CHIP_FAMILY = BOARD_CONFIG.get("build.mcu", "esp32")
 
+
+def merge_bin(source, target, env):
+    # The list contains all extra images (bootloader, partitions, eboot) and
+    # the final application binary
+    flash_images = env.Flatten(env.get("FLASH_EXTRA_IMAGES", [])) + ["$ESP32_APP_OFFSET", APP_BIN]
+
+    # Run esptool to merge images into a single binary
+    env.Execute(
+        " ".join(
+            [
+                "$PYTHONEXE",
+                "$OBJCOPY",
+                "--chip",
+                CHIP_FAMILY,
+                "merge_bin",
+                # "--fill-flash-size",
+                # BOARD_CONFIG.get("upload.flash_size", "4MB"),
+                "-o",
+                MERGED_BIN,
+            ]
+            + flash_images
+        )
+    )
