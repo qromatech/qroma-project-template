e2db6a166a1d34266b7071a752202541d77b2b7f
Albert Boehmler <ajboehmler@gmail.com>
2024-01-14 11:05:04 -0500

    Build project ID into firmware details response

--- firmware/esp32/qroma-project/src/qroma-project/boards/qroma-boards.cpp
+++ firmware/esp32/qroma-project/src/qroma-project/boards/qroma-boards.cpp
@@ -3,6 +3,7 @@
 
 
 void populateBoardFirmwareDetails(FirmwareDetails * firmwareDetails) {
+  strncpy(firmwareDetails->projectName, QROMA_PROJECT_NAME, sizeof(firmwareDetails->projectName));
   strncpy(firmwareDetails->version, LIB_VER, sizeof(firmwareDetails->version));
   strncpy(firmwareDetails->buildTime, __DATE__ " " __TIME__, sizeof(firmwareDetails->buildTime));
 }
--- firmware/esp32/qroma-project/src/qroma-project/lib_ver.h
+++ firmware/esp32/qroma-project/src/qroma-project/lib_ver.h
@@ -2,7 +2,9 @@
 #ifndef LIB_VER_QROMA_FIRMWARE_H
 #define LIB_VER_QROMA_FIRMWARE_H
 
-#define LIB_VER  "0.0.1"
+#define LIB_VER             "0.0.1"
+#define QROMA_PROJECT_NAME  "{{ qroma_project.project_id }}"
+
 
 
 #endif
