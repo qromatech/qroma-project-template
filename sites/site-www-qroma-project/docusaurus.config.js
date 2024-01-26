14ba7b10650dd615a4a843db6ebefdbb264fda7f
Albert Boehmler <ajboehmler@gmail.com>
2024-01-26 07:43:22 -0500

    text fixes

--- sites/site-www-qroma-project/docusaurus.config.js
+++ sites/site-www-qroma-project/docusaurus.config.js
@@ -144,7 +144,7 @@ const config = {
             label: 'qroma.dev',
           },
         ],
-        copyright: `Copyright ${new Date().getFullYear()} {{ qroma_project.project_id }} - built with Docusaurus.`,
+        copyright: `Copyright ${new Date().getFullYear()} - {{ qroma_project.project_id }} - built with Docusaurus`,
       },
       prism: {
         theme: prismThemes.github,
